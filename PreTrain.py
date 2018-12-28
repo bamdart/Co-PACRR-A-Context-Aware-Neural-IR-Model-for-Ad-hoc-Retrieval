466e18eafb16175ca6e026478dbcdb3bf88cc237
bamdart <bamdart@gmail.com>
2018-12-28 16:22:16 +0800

    add w2v model

---
+++ PreTrain.py
@@ -0,0 +1,68 @@
+import sys
+import os
+import numpy as np
+import time
+import gensim
+from sklearn.metrics.pairwise import cosine_similarity
+
+ModelPath = "./Model/"
+TrainingQueryPath = "./data/Query_Training/"
+DocumentPath = "./data/100000Doc/"
+
+LoadWord2VecModel = True
+
+# Word2Vec param
+Word2Vec_size = 300
+Word2Vec_window = 10
+Word2Vec_worker = 1000
+Word2Vec_iter = 100
+
+def ConsoleLog(msg):
+    print("[", time.strftime('%H:%M:%S', time.localtime(time.time())), "] " + msg)
+
+def Loading():
+    query_filename = []
+    query_sentence = []
+    document_filename = []
+    document_sentence = []
+    documents = []
+    queries = []
+
+    for filename in os.listdir(TrainingQueryPath):
+        query_filename += [filename]
+        with open(TrainingQueryPath + filename) as f:
+            query = ' '.join([word for line in f.readlines() for word in line.split()[:-1]])
+            queries.append(query)
+            query_sentence += [query.split()]
+
+    for filename in os.listdir(DocumentPath):
+        document_filename += [filename]
+        with open(DocumentPath + filename) as f:
+            doc = ' '.join([word for line in f.readlines()[3:] for word in line.split()[:-1]])
+            documents.append(doc)
+            document_sentence += [doc.split()]
+    
+    return query_filename, query_sentence, queries, document_filename, document_sentence, documents
+
+
+if __name__ == '__main__':
+    # 讀取query, document等data
+    ConsoleLog("Loading")
+    # query_filename : query的檔案名稱list
+    # query_sentence : query將每個檔案分成一層list，再將每個list裡面的字分成list (string of list of list)
+    # queries :　query將每個檔案分成一層list　(string of list)
+    # document_filename : document的檔案名稱list
+    # document_sentence : document將每個檔案分成一層list，再將每個list裡面的字分成list (string of list of list)
+    # documents : document將每個檔案分成一層list　(string of list)
+    query_filename, query_sentence, queries, document_filename, document_sentence, documents = Loading()
+
+    ConsoleLog("Word2Vec")
+    if(LoadWord2VecModel):
+        w2v_model = gensim.models.Word2Vec.load(ModelPath + "word2vec.model")
+    else:
+        train_sentences = document_sentence + query_sentence
+        w2v_model = gensim.models.Word2Vec(sentences = train_sentences, min_count = 1, 
+                            size = Word2Vec_size, window = Word2Vec_window, workers = Word2Vec_worker, iter = Word2Vec_iter, hs = 0)
+        w2v_model.save(ModelPath + "word2vec.model")
+
+    
