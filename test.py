e0b49ece617a03e85087001f692021e212f86061
bamdart <bamdart@gmail.com>
2019-01-03 23:59:09 +0800

    fix glob doc_mat_dir

--- test.py
+++ test.py
@@ -37,10 +37,14 @@ def _load_doc_mat_desc(qids, qid_topic_idf, qid_desc_idf):
             mi.append(desc_idf_arr[didxs])
         qid_term_idf[qid] = np.concatenate(mi, axis=0).astype(np.float32)
 
-        cwid_list = glob.glob('/desc_doc_mat/%d/'%(qid))
+        cwid_list = glob.glob('simmat/desc_doc_mat/' + str(qid) + '/*.npy')
+        # print('simmat/desc_doc_mat/' + str(qid) + '/')
+        # print(cwid_list)
         for cwid in cwid_list: ##############  這邊要改成讀取資料夾內的所有npy
-            topic_cwid_f = doc_mat_dir + '/topic_doc_mat/%d/%s.npy'%(qid, cwid)
-            desc_cwid_f = doc_mat_dir + '/desc_doc_mat/%d/%s.npy'%(qid, cwid)
+            # topic_cwid_f = doc_mat_dir + '/topic_doc_mat/%d/%s.npy'%(qid, cwid)
+            # desc_cwid_f = doc_mat_dir + '/desc_doc_mat/%d/%s.npy'%(qid, cwid)
+            topic_cwid_f = cwid
+            desc_cwid_f = cwid
             topic_mat, desc_mat = np.empty((0,0), dtype=np.float32), np.empty((0,0), dtype=np.float32)
             if usetopic:
                 topic_mat = np.load(topic_cwid_f)
